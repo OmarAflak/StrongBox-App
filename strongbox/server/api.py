0ac7e87730c63bc2397e48414aa00da342e911de
Omar Aflak <aflakomar@gmail.com>
2021-12-11 19:40:44 +0000

    format API response

--- strongbox/server/api.py
+++ strongbox/server/api.py
@@ -1,9 +1,14 @@
-from typing import Optional
+from typing import Generic, Optional, TypeVar
 from fastapi import FastAPI, Body
+from dataclasses import dataclass
 from fastapi.middleware.cors import CORSMiddleware
+from cryptography.fernet import InvalidToken
+from strongbox.locker.io import IO
 from strongbox.locker.locker import Account
 import strongbox.server.utils as utils
 
+_ERROR_WRONG_PASSWORD = "Wrong password"
+_ERROR_URL_NOT_FOUND = "URL not found"
 
 app = FastAPI()
 app.add_middleware(
@@ -15,13 +20,38 @@ app.add_middleware(
 )
 
 
+T = TypeVar("T", bound=IO)
+
+
+@dataclass
+class APIResponse(Generic[T]):
+    success: bool
+    message: Optional[str] = None
+    data: Optional[T] = None
+
+    @classmethod
+    def on_success(cls, data: T) -> 'APIResponse[T]':
+        return cls(success=True, message=None, data=data)
+
+    @classmethod
+    def on_fail(cls, message: Optional[str] = None) -> 'APIResponse[T]':
+        return cls(success=False, message=message, data=None)
+
+
 @app.post("/account")
 def get_account(
     profile: str = Body(None),
     password: str = Body(None),
     url: str = Body(None)
-) -> Optional[Account]:
-    return utils.get_account(profile, password, url)
+) -> APIResponse[Account]:
+    try:
+        account = utils.get_account(profile, password, url)
+        if account:
+            return APIResponse[Account].on_success(account)
+        else:
+            return APIResponse[Account].on_fail(_ERROR_URL_NOT_FOUND)
+    except InvalidToken:
+        return APIResponse[Account].on_fail(_ERROR_WRONG_PASSWORD)
 
 
 @app.post("/account/new")
@@ -29,5 +59,9 @@ def add_account(
     profile: str = Body(None),
     password: str = Body(None),
     account: Account = Body(None)
-):
-    utils.add_account(profile, password, account)
+) -> APIResponse[bool]:
+    try:
+        utils.add_account(profile, password, account)
+        return APIResponse[bool].on_success(True)
+    except InvalidToken:
+        return APIResponse[bool].on_fail(_ERROR_WRONG_PASSWORD)
