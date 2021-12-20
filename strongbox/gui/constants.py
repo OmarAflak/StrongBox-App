adc3e3338259bc3a9c7e41eb470bd69ffc1e3f53
Omar Aflak <aflakomar@gmail.com>
2021-12-20 23:44:15 +0000

    helpers

--- strongbox/gui/constants.py
+++ strongbox/gui/constants.py
@@ -5,7 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def _get_int(key: str, default: int) -> int:
+    return int(os.environ.get(key, default))
+
+
+def _get_string(key: str, default: str) -> str:
+    return int(os.environ.get(key, default))
+
+
 class Constants(ABC):
-    APP_TITLE: str = os.environ.get("APP_TITLE", "StrongBox")
-    SERVER_HOST: str = os.environ.get("SERVER_HOST", "127.0.0.1")
-    SERVER_PORT: int = int(os.environ.get("SERVER_PORT", 8000))
+    APP_TITLE: str = _get_string("APP_TITLE", "StrongBox")
+    SERVER_HOST: str = _get_string("SERVER_HOST", "127.0.0.1")
+    SERVER_PORT: int = _get_int("SERVER_PORT", 8000)
